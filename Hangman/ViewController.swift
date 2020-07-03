8fbe1dff8ebec0087112a6e0dd0e3a9ef7991455
jon emery <jonemery111@gmail.com>
2020-07-02 18:00:36 -0700

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by jon emery on 7/2/20.
+//  Copyright © 2020 jon emery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
