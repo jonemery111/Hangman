ffe6b076583d580c0eb7ab31893da3df1b664a00
jon emery <jonemery111@gmail.com>
2020-07-23 19:05:00 -0700

    on git now

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var correctWordTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    var wordlist = ["decade", "century","farming","opportunity","childhood","protestors","coronavirus","unemployment","quarantine","inspireidaho","hashtag"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
